361849c924fe1138fcd6d1fd1da6abe4611212e6
sharkshima <32904759+sharkshima@users.noreply.github.com>
2018-05-15 00:06:11 +0900

    Update ping.py

--- ping.py
+++ ping.py
@@ -11,22 +11,22 @@ eを入力することでping開始
 while True :
     N = input()
     if N=="e":
-    	break
-	ping.append(N)
+        break
+    ping.append(N)
 
 flg = 0
 while True:
-	try:
-		sb.check_output('ping -n 1 {}'.format(ping[flg]))
-	except:
-	    print('{}へのping失敗'.format(ping[flg]))
-	else:
-		print('{}へのping成功'.format(ping[flg]))
+    try:
+        sb.check_output('ping -n 1 {}'.format(ping[flg]))
+    except:
+        print('{}へのping失敗'.format(ping[flg]))
+    else:
+        print('{}へのping成功'.format(ping[flg]))
 
-	if flg==(len(ping)-1):
-		flg = 0
-	else:
-		lg +=1
+    if flg==(len(ping)-1):
+        flg = 0
+    else:
+        flg +=1
 	
-	time.sleep(1)
+    time.sleep(1)
 ''' とりあえずCtrl+cで終了させといて～ '''
